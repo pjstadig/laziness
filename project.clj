e71e20418a11fdbb8329127f3a0e0740b48f02d0
Paul Stadig <paul@stadig.name>
2012-02-09 18:54:24 -0500

    Update description in project.clj.

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
-(defproject laziness "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
+(defproject laziness "1.0.0"
+  :description "My Clojure/West presentation on the Clojure laziness constructs and common pitfalls."
   :dependencies [[org.clojure/clojure "1.3.0"]])
