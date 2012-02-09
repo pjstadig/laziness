33125ee4b3204d0f4afb3928643fb93134ea503d
Paul Stadig <paul@stadig.name>
2012-02-09 18:50:25 -0500

    Initial project structure.

---
+++ src/laziness/core.clj
@@ -0,0 +1,1 @@
+(ns laziness.core)
---
+++ test/laziness/test/core.clj
@@ -0,0 +1,6 @@
+(ns laziness.test.core
+  (:use [laziness.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
