a2aa5cb8bbf5c39731bdb0eadceb693ee0bfa837
Paul Stadig <paul@stadig.name>
2012-03-12 21:02:19 -0400

    Bump to 1.4.

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject laziness "1.0.0"
-  :description "My Clojure/West presentation on the Clojure laziness constructs and common pitfalls."
-  :dependencies [[org.clojure/clojure "1.3.0"]]
+  :description "My Clojure/West presentation on the Clojure laziness constructs
+  and common pitfalls."
+  :dependencies [[org.clojure/clojure "1.4.0-beta3"]]
   :jvm-opts ["-Xmx100m"]
   :aot [laziness.case-study2])
